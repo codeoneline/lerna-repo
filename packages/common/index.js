663ef502f5eb0dde43aeed1ef64a1a33585bb69d
codeoneline <mutouji1@163.com>
2020-09-21 16:49:34 +0800

    fourth

--- packages/common/index.js
+++ packages/common/index.js
@@ -1,4 +1,6 @@
 module.exports = () => {
   console.log("hello from common 12");
   console.log("hello from common 13");
+  console.log("hello from common 14");
+  console.log("hello from common 15");
 };
